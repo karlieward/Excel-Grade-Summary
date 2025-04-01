c23838a1ca13180457df4ed756893bc3700c6a01
pinkrach <166764867+pinkrach@users.noreply.github.com>
2025-03-31 18:36:14 -0600

    Rachel
    consolidates code so it can work for either of the inputs

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ class Student:
         self.className = className
 
 # Open the excel sheet to extract information
-wbOriginal = openpyxl.load_workbook("Poorly_Organized_Data_1.xlsx")
+wbOriginal = openpyxl.load_workbook("Poorly_Organized_Data_2.xlsx")
 wsOriginal = wbOriginal.active
 
-# List of studnet objects
+# List of student objects
 lstStudents = []
+
+# Class Dictionary
+dictionaryClass = {}
 
 # Sets active row as the second row to avoid the column headers
 activeRow = 2
@@ -38,46 +41,33 @@ while True:
 
     oStudent = Student(class_name, studentGrade, last, first, studentID) # Create new student 
     lstStudents.append(oStudent)  # Append student to the list
+
+    if class_name in dictionaryClass:
+        dictionaryClass[class_name].append(oStudent)
+    else:
+        dictionaryClass[class_name] = []
+        dictionaryClass[class_name].append(oStudent)
+
     activeRow += 1
-
-# for item in lstStudents:
-#     print(item.get_info())
-
 
 # Set up the beginning columns for each new sheet
 myWorkbook = Workbook()
-wsAlg = myWorkbook.create_sheet("Algebra")
-wsAlg["A1"] = "Last Name"
-wsAlg["B1"] = "First Name"
-wsAlg["C1"] = "Student ID"
-wsAlg["D1"] = "Grade"
 
-wsTrig = myWorkbook.create_sheet("Trigonometry")
-wsTrig["A1"] = "Last Name"
-wsTrig["B1"] = "First Name"
-wsTrig["C1"] = "Student ID"
-wsTrig["D1"] = "Grade"
+for key in dictionaryClass:
+    newWS = myWorkbook.create_sheet(key)
+    newWS["A1"] = "Last Name"
+    newWS["B1"] = "First Name"
+    newWS["C1"] = "Student ID"
+    newWS["D1"] = "Grade"
 
-wsGeo = myWorkbook.create_sheet("Geometry")
-wsGeo["A1"] = "Last Name"
-wsGeo["B1"] = "First Name"
-wsGeo["C1"] = "Student ID"
-wsGeo["D1"] = "Grade"
-
-wsCalc = myWorkbook.create_sheet("Calculus")
-wsCalc["A1"] = "Last Name"
-wsCalc["B1"] = "First Name"
-wsCalc["C1"] = "Student ID"
-wsCalc["D1"] = "Grade"
-
-wsStats = myWorkbook.create_sheet("Statistics")
-wsStats["A1"] = "Last Name"
-wsStats["B1"] = "First Name"
-wsStats["C1"] = "Student ID"
-wsStats["D1"] = "Grade"
-
-
-
+    count = 2
+    for student in dictionaryClass[key]:
+        newWS.cell(row=count, column=1).value = student.last
+        newWS.cell(row=count, column=2).value = student.first
+        newWS.cell(row=count, column=3).value = student.id
+        newWS.cell(row=count, column=4).value = student.grade
+        
+        count += 1
 
 myWorkbook.save(filename="formatted_grades.xlsx")
 myWorkbook.close()
